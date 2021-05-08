2f8efa39e29029d8a865a7b7ec30125da9701e84
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-08 22:59:07 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+// 20% of user's browser does not support es6 which fails to render code through import/export where as the require function which comes from nodemodules is preferable as an alternative if required.
 import React from "react";
 import ReactDOM from "react-dom";
 
+//  Even below one works
+// import pie, { doublePi, triplePi } from "./math";
+// {pie} --> works
+import * as pi from "./math"; // but this is dicouraged because its good to import what we require.
+
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
---
+++ src/math.js
@@ -0,0 +1,13 @@
+const pi = 3.1415962;
+
+const doublePi = function () {
+  return pi * 2;
+};
+
+const triplePi = function () {
+  return pi * 3;
+};
+
+export default pi;
+
+export { doublePi, triplePi };
